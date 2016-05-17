706adf8c119635cdc957db74e1106e0a56e07f11
Yves-Laurent <yveslaurent.komsamo@gmail.com>
2016-05-17 12:34:52 +0100

    Clarifying the examples.

--- StringGPy/examples/example.py
+++ StringGPy/examples/example.py
@@ -12,17 +12,21 @@ Regression Example
 from StringGPy.samplers.regression import SGPRegressor
 # Create the model.
 kernel_type = "se"          # Many kernels are available including Matern 1/2, 3/2, 5/2, RQ, and stationary spectral kernels.
-all_x = None                # Your inputs here 
-all_y = None                # Your outputs here
+                            #   See the function "kernel" in src/kernel_utilities.c for a complete list.
+all_x = None                # Your inputs here. Should be a two dimensional np.ndarray of shape (N, d) that contains all training and testing inputs.
+all_y = None                # Your outputs here. Should be a one dimensional np.ndarray of shape (N) that contains all training and testting labels. 
+                            #   In binary classification, by convention the labels should be +/- 1.
 n_train = None              # Your number of training samples here.
-y_train = all_y[:n_train]   # Trainging labels.
-link_f_type = "sum"         # Type of link function.
+y_train = all_y[:n_train]   # Trainging labels. This is what the model will see at training time, although it will infer values of the latent function at 
+                            #   training and test inputs.
+link_f_type = "sum"         # Type of link function. Implemented choices are "sum", "prod", and "prod_sum" (mixture of sum and product).
 hyper_prior_type = "log"    # Log-Gaussian or Sigmoid-Gaussian prior on hyper-parameters.
 scaling_hypers = None       # A scaling vector of hyper-parameters prior, which corresponds to half the mode.
 plot = False                # Whether to display real-time plots of learned functions.
 infer_change_pts = True     # Whether to infer the location of change-points in string kernel memberships.
+should_print = True         # Whether to log the evolution of the Markov Chain (every 100 samples).
 
-m = SGPRegressor(all_x, y_train, n_train, kernel_type, scaling_hypers, link_f_type, True, hyper_prior_type, plot,\
+m = SGPRegressor(all_x, y_train, n_train, kernel_type, scaling_hypers, link_f_type, should_print, hyper_prior_type, plot,\
     infer_change_pts=infer_change_pts)
     
 # Run the Markov Chain / Train
@@ -37,7 +41,8 @@ Binary Classification Example
 from StringGPy.samplers.binary_classification import SGPBinaryClassifier
 
 # Create the model.
-m = SGPBinaryClassifier(all_x, y_train, n_train, kernel_type, scaling_hypers, link_f_type, True, hyper_prior_type, plot,\
+#   Remember, in binary classification, by convention the labels in "y_train" should be +/- 1.
+m = SGPBinaryClassifier(all_x, y_train, n_train, kernel_type, scaling_hypers, link_f_type, should_print, hyper_prior_type, plot,\
     infer_change_pts=infer_change_pts)
 
 # Run the Markov Chain / Train  
